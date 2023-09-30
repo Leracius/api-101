310e48585e52eabe7afd4660b7da97d3dcccb7ad
Axel Quintana <lucasaxelqg15@gmail@email.com>
2023-09-30 03:59:01 -0300

    patching mg

--- controllers/controller.ts
+++ controllers/controller.ts
@@ -20,7 +20,6 @@ export const postComments = async(req : Request, res: Response) =>{
     res.status(200).json({
         msg : "Se guardo tu comentario, saludos desde el back!",
         timestamp: new Date().toLocaleString(),
-        comment
     });
 }
 
@@ -36,5 +35,35 @@ export const getComments = async (req: Request, res: Response) => {
     } catch (error) {
       console.error("Error al obtener los comentarios:", error);
       res.status(500).json({ error: "Error al obtener los comentarios" });
+    }
+  };
+
+  export const incrementarMg = async (req: Request, res: Response) => {
+    try {
+      const { nombre, msg, createdAt } = req.body;
+  
+      const comentario = await Comment.findOne({ nombre, msg, createdAt });
+  
+      if (!comentario) {
+        return res.status(404).json({ error: 'Comentario no encontrado' });
+      }
+  
+      if (comentario.mg !== undefined) {
+        comentario.mg += 1;
+      } else {
+        console.error("La propiedad 'mg' no está definida en el comentario:", comentario);
+        return res.status(500).json({ error: "Error al incrementar 'mg'" });
+      }
+  
+      await comentario.save();
+  
+      res.json({
+        msg: "Propiedad 'mg' incrementada exitosamente",
+        timestamp: new Date().toLocaleString(),
+        comentario,
+      });
+    } catch (error) {
+      console.error("Error al incrementar 'mg':", error);
+      res.status(500).json({ error: "Error al incrementar 'mg'" });
     }
   };
--- routes/home.ts
+++ routes/home.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { getComments, gethome, postComments } from "../controllers/controller";
+import { getComments, gethome, incrementarMg, postComments } from "../controllers/controller";
 
 const router = Router()
 
 router.get('/', [
     
 ] ,gethome)
+
+router.patch('/comments', [
+    
+] ,incrementarMg);
 
 router.post('/comments',[
 
